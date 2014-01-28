d9c47c380f0b166d25b87a5c38e3c8c05364e4a6
ogre <vincent@ogre.be>
2014-01-28 20:59:36 +0100

    update

--- back/curl.php
+++ back/curl.php
@@ -5,7 +5,7 @@
         curl_setopt($ch, CURLOPT_URL,$url);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_VERBOSE, 0);
-        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POST, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         # Send post
         $result = curl_exec($ch);
